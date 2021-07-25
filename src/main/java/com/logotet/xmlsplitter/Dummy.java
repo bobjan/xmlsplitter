032577421449857ee09cbaf16d1ef06973d869b4
bobjan <bobjan@sbb.rs>
2021-07-25 19:57:41 +0200

    not part of the project; used to test IntelliJIdea plugin

---
+++ src/main/java/com/logotet/xmlsplitter/Dummy.java
@@ -0,0 +1,9 @@
+package com.logotet.xmlsplitter;
+
+/**
+ * @author : Boban Jankovic
+ * @since : 2021
+ **/
+public class Dummy {
+//    not part of the project; used to test IntelliJIdea plugin
+}
